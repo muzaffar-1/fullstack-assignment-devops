dd63243115abec3dde4af397efad6870e1f26e2a
muzaffar-1 <mdmuzaffar.qureshi2@gmail.com>
2026-02-27 09:15:40 +0000

    fix: resolve DisallowedHost using environment variables

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -30,7 +30,7 @@ FRONTEND_URL = os.environ["FRONTEND_URL"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
 
 # Application definition
 
